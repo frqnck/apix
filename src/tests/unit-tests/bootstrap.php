308d9f1bceadec6a165f7590e29304118c8980a2
Franck Cassedanne <franck@ouarz.net>
2012-05-24 13:54:41 +0100

    CI trial.

--- src/tests/unit-tests/bootstrap.php
+++ src/tests/unit-tests/bootstrap.php
@@ -18,8 +18,12 @@ define('APP_TOPDIR', realpath(__DIR__ . '/../../php'));
 define('APP_LIBDIR', realpath(__DIR__ . '/../../../vendor/php'));
 define('APP_TESTDIR', realpath(__DIR__ . '/php'));
 
+$app_libdir = realpath(__DIR__ . '/../../../vendor/php');
+
+require_once($app_libdir . '/psr0.autoloader.php');
+
 // step 2: find the autoloader, and install it
-require_once(APP_LIBDIR . '/psr0.autoloader.php');
+#require_once(APP_LIBDIR . '/psr0.autoloader.php');
 
 // step 3: add the additional paths to the include path
 psr0_autoloader_searchFirst(APP_LIBDIR);
