5cf087974793ba46217f336c2f4918a574af1b9d
Franck Cassedanne <franck@ouarz.net>
2012-06-07 21:14:21 +0100

    Working on Response

--- src/php/Zenya/Api/Router.php
+++ src/php/Zenya/Api/Router.php
@@ -69,12 +69,13 @@ class Router
      *
      * @param array $rules
      * @param array $defaults
+     * @throws \InvalidArgumentException 500
      */
     public function __construct(array $rules, array $defaults=array())
     {
         foreach ($rules as $k => $v) {
             if ( is_int($k) ) {
-                throw new Exception("Invalid rules array specified (not associative)", 500);
+                throw new \InvalidArgumentException("Invalid rules array specified (not associative)", 500);
             }
             $this->_rules[$k] = $v;
         }
