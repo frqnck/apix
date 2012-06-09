b0b56c3acead39fee5578c97fa035c68fc142b49
Franck Cassedanne <franck@ouarz.net>
2012-06-09 15:08:39 +0100

    Created Config.php

--- src/php/Zenya/Api/Config.php
+++ src/php/Zenya/Api/Config.php
@@ -5,90 +5,96 @@ namespace Zenya\Api;
 class Config #extends Pimple
 {
 
-    protected $config = array(
-            'org' => "Zenya",
+    protected $default = array(
+        'org' => 'Zenya',
+        'version'   => '1.0',
 
-            'route_prefix' => '@^(/index.php)?/api/v(\d*)@i', // regex
+        // output
+        'rootNode' => 'zenya',
+        'sign'  => true,
+        'debug' => true,
 
-            // routes
-            'routes' => array(
-                #'/:controller/paramName/:paramName/:id' => array(),
-                #'/:controller/test' => array('class'=>'test'),
+        // routes
+        'route_prefix' => '@^(/index.php)?/api/v(\d*)@i', // regex
+        
+        'routes' => array(
+            #'/:controller/paramName/:paramName/:id' => array(),
+            #'/:controller/test' => array('class_name'=>'test'),
 
-                '/category/:param1/:param2/:param3' => array(
-                    'controller' => 'Category',
-
-                ),
-
-                '/:controller/:param1/:param2' => array(
-                    #'controller' => 'BlankResource',
-                    #'className' => 'Zenya\Api\Fixtures\BlankResource',
-                    'classArgs' => array('classArg1' => 'test1', 'classArg2' => 'test2'))
+           '/help/:resource/:http_method/:filters' => array(
+                'controller' => 'help',
+                #'method'=>'GET'
             ),
 
-            // need DIC here!!
-            'listeners' => array(
-                // pre-processing stage
-                'early' => array(
-                    'new Listener\Auth',
-                    'new Listener\Acl',
-                    'new Listener\Log',
-                    'new Listener\Mock'
-                ),
-
-                // post-processing stage
-                'late'=>array(
-                    'new Listener\Log',
-                ),
+            '/category/:param1/:param2/:param3' => array(
+                'controller' => 'Category',
             ),
 
-            // -- advanced options --
-            'auth' => array(
-                    'type'=>'Basic',
-                    #'type'=>'Digest',
-                ),
-            'internals' => array(
-                // OPTIONS
-                'help' => array(
-                        'class'     => 'Zenya\Api\Resource\Help',
-                        'classArgs' => '&$this',
-                        'args'      => array('params'=>'dd')
-                    ),
-                // HEAD
-                'test' => array(
-                        'class'     => 'Zenya\Api\Resource\Test',
-                        'classArgs' => '&$this',
-                        'args'      => array()
+            '/:controller/:param1/:param2' => array(
+                #'controller' => 'BlankResource',
+                #'class_name' => 'Zenya\Api\Fixtures\BlankResource',
+                'class_args' => array('classArg1' => 'test1', 'classArg2' => 'test2'))
+        ),
+
+        // need a DIC !
+        'listeners' => array(
+            // pre-processing stage
+            'early' => array(
+                'new Listener\Auth',
+                'new Listener\Acl',
+                'new Listener\Log',
+                'new Listener\Mock'
+            ),
+
+            // post-processing stage
+            'late'=>array(
+                'new Listener\Log',
+            ),
+        ),
+
+        // -- advanced options --
+        'auth' => array(
+                'type'=>'Basic',
+                #'type'=>'Digest',
+            ),
+        'resources_default' => array(
+            // OPTIONS
+            'help' => array(
+                    'class_name'    => 'Zenya\Api\Resource\Help',
+                    'class_args'    => null, //&$this
+                    'args'          => array(
+                        # 'method'  => 'GET',
+                        # 'name'    => $this->route->getControllerName(),
+                        # 'resource'  => $this->server->getResource( $route->getControllerName() ),
+                        # 'params'    => $route->getParams(),
                     )
-            )
-        );
+                ),
+            // HEAD
+            'test' => array(
+                    'class_name'    => 'Zenya\Api\Resource\Test',
+                    'class_args'    => null,
+                    'args'          => array()
+                )
+        )
+    );
 
-    public function __set($key, $value)
+    private $injected = array();
+
+    public function __construct(array $config=array())
     {
-        $this->params[$key] = $value;
+        $this->config = $this->default+$config;
+        #Server::d($this);
     }
 
-    public function __get($key)
+    public function injet($key, $mixed)
     {
-        if (!isset($this->params[$key])) {
-            throw new InvalidArgumentException(sprintf('Value "%s" is not defined.', $key));
-        } if (is_callable($this->params[$key])) {
-            return $this->params[$key]($this);
-        } else {
-            return $this->params[$key];
-        }
+        return $this->injected[$key] = $mixed;
     }
 
-    public function asShared($callable)
+    public function getConfig()
     {
-        return function ($c) use ($callable) {
-                    static $object;
-                    if (is_null($object)) {
-                        $object = $callable($c);
-                    }
-
-                    return $object;
-                };
+        #$this->config['resources_default']['help']['class_args'] = $this->injected['server'];
+        return $this->config;
     }
 
 }
