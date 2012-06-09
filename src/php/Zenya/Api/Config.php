9b6f2f8f7da96265f77921334023f836c6c7c633
Franck Cassedanne <franck@ouarz.net>
2012-06-09 19:18:27 +0100

    Worked on Server

--- src/php/Zenya/Api/Config.php
+++ src/php/Zenya/Api/Config.php
@@ -5,85 +5,13 @@ namespace Zenya\Api;
 class Config #extends Pimple
 {
 
-    protected $default = array(
-        'org' => 'Zenya',
-        'version'   => '1.0',
-
-        // output
-        'rootNode' => 'zenya',
-        'sign'  => true,
-        'debug' => true,
-
-        // routes
-        'route_prefix' => '@^(/index.php)?/api/v(\d*)@i', // regex
-        
-        'routes' => array(
-            #'/:controller/paramName/:paramName/:id' => array(),
-            #'/:controller/test' => array('class_name'=>'test'),
-
-           '/help/:resource/:http_method/:filters' => array(
-                'controller' => 'help',
-                #'method'=>'GET'
-            ),
-
-            '/category/:param1/:param2/:param3' => array(
-                'controller' => 'Category',
-            ),
-
-            '/:controller/:param1/:param2' => array(
-                #'controller' => 'BlankResource',
-                #'class_name' => 'Zenya\Api\Fixtures\BlankResource',
-                'class_args' => array('classArg1' => 'test1', 'classArg2' => 'test2'))
-        ),
-
-        // need a DIC !
-        'listeners' => array(
-            // pre-processing stage
-            'early' => array(
-                'new Listener\Auth',
-                'new Listener\Acl',
-                'new Listener\Log',
-                'new Listener\Mock'
-            ),
-
-            // post-processing stage
-            'late'=>array(
-                'new Listener\Log',
-            ),
-        ),
-
-        // -- advanced options --
-        'auth' => array(
-                'type'=>'Basic',
-                #'type'=>'Digest',
-            ),
-        'resources_default' => array(
-            // OPTIONS
-            'help' => array(
-                    'class_name'    => 'Zenya\Api\Resource\Help',
-                    'class_args'    => null, //&$this
-                    'args'          => array(
-                        # 'method'  => 'GET',
-                        # 'name'    => $this->route->getControllerName(),
-                        # 'resource'  => $this->server->getResource( $route->getControllerName() ),
-                        # 'params'    => $route->getParams(),
-                    )
-                ),
-            // HEAD
-            'test' => array(
-                    'class_name'    => 'Zenya\Api\Resource\Test',
-                    'class_args'    => null,
-                    'args'          => array()
-                )
-        )
-    );
-
+    private $config = array();
     private $injected = array();
 
     public function __construct(array $config=array())
     {
-        $this->config = $this->default+$config;
-        #Server::d($this);
+        $this->config = $config+$this->getDefaults();
+        #Server::d($this->config);
     }
 
     public function injet($key, $mixed)
@@ -95,6 +23,94 @@ class Config #extends Pimple
     {
         #$this->config['resources_default']['help']['class_args'] = $this->injected['server'];
         return $this->config;
+    }
+
+    public function getDefaults()
+    {
+        return array(
+            'org' => 'Zenya',
+            'version'   => '1.0',
+
+            // output
+            'rootNode' => 'zenya',
+            'sign'  => true,
+            'debug' => true,
+
+            // routes
+            'route_prefix' => '@^(/index.php)?/api/v(\d*)@i', // regex
+            
+            'routes' => array(
+                #'/:controller/paramName/:paramName/:id' => array(),
+                #'/:controller/test' => array('class_name'=>'test'),
+
+               '/help/:resource/:http_method/:filters' => array(
+                    'controller' => 'help',
+                    #'method'=>'GET'
+                ),
+
+                '/category/:param1/:param2/:param3' => array(
+                    'controller' => 'Category',
+                ),
+
+                '/:controller/:param1/:param2' => array(
+                    #'controller' => 'BlankResource',
+                    #'class_name' => 'Zenya\Api\Fixtures\BlankResource',
+                    'class_args' => array('classArg1' => 'test1', 'classArg2' => 'test2')
+                )
+            ),
+            // format negociation
+            'format_negotiation' => array(
+                'default'        => 'json',
+                'controller_ext' => true, // true or false (e.g. resource.json)
+                'request_chain'  => isset($_REQUEST['format']) ? $_REQUEST['format'] : false,
+                'http_accept'    => true, // true or false
+            ),
+
+            // listeners
+            'listeners' => array(
+                // pre-processing stage
+                'early' => array(
+                    'new Listener\Auth',
+                    'new Listener\Acl',
+                    'new Listener\Log',
+                    'new Listener\Mock'
+                ),
+
+                // post-processing stage
+                'late'=>array(
+                    'new Listener\Log',
+                ),
+            ),
+
+            // auth shoud be in listeners (todo: DIC)
+            'auth' => array(
+                    'type'=>'Basic',
+                    #'type'=>'Digest',
+                ),
+            
+            // resources
+            'resources' => null, // user defined
+
+            'resources_default' => array(
+                // OPTIONS
+                'help' => array(
+                        'class_name'    => 'Zenya\Api\Resource\Help',
+                        'class_args'    => null, //&$this
+                        'args'          => array(
+                            # 'method'  => 'GET',
+                            # 'name'    => $this->route->getControllerName(),
+                            # 'resource'  => $this->server->getResource( $route->getControllerName() ),
+                            # 'params'    => $route->getParams(),
+                        )
+                    ),
+                // HEAD
+                'test' => array(
+                        'class_name'    => 'Zenya\Api\Resource\Test',
+                        'class_args'    => null,
+                        'args'          => array()
+                    )
+            )
+        );
     }
 
 }
