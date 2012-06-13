7a4aa1d02331ce01a67ed5e293dbe9f66435d829
Franck Cassedanne <franck@ouarz.net>
2012-06-13 19:55:53 +0100

    Config.php

--- src/tests/unit-tests/php/Zenya/Api/ServerTest.php
+++ src/tests/unit-tests/php/Zenya/Api/ServerTest.php
@@ -5,11 +5,13 @@ namespace Zenya\Api;
 class ServerTest extends \PHPUnit_Framework_TestCase
 {
 
-    protected $server;
+    protected $server, $request;
 
     protected function setUp()
     {
-        $this->server = new Server;
+        $this->request = $this->getMockBuilder('Zenya\Api\Request')->disableOriginalConstructor()->getMock();
+
+        $this->server = new Server(new Config, $this->request);
     }
 
     protected function tearDown()
@@ -33,48 +35,26 @@ class ServerTest extends \PHPUnit_Framework_TestCase
         $this->server->run();
     }
 
-    public function testSetRouting()
+    public function testGetResults()
     {
-        $m = $this->getMock('Zenya\Api\Request');
-
-        $m->expects($this->any())
-            ->method('getUri')
-            ->will($this->returnValue('/test.php'));
-
-        $m->expects($this->any())
-            ->method('getMethod')
-            ->will($this->onConsecutiveCalls('GET', 'POST', 'PUT', 'OPTIONS' ));
-
-        $this->assertObjectHasAttribute('route', $this->server);
-        $this->assertSame(null, $this->server->route);
-
-        $this->server->setRouting(new $m);
-        $this->assertInstanceOf('Zenya\Api\Router', $this->server->route);
-
-       # $this->assertSame('Zenya\Api\Router', $this->server->path);
-
-        #$this->assertSame(array('foo', 'bar'), $this->server->extractExtension('foo.bar'));
-        #$this->assertSame(array('f', 'bar'), $this->server->extractExtension('f.o.o.bar'));
-        #$this->assertSame(false, $this->server->extractExtension('foobar'));
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+        $this->assertSame(null, $this->server->getResults());
     }
+
 
     public function testGetFormatFromHttpAccept()
     {
-        $m = $this->getMock('Zenya\Api\Request');
+        $this->request->expects($this->any())->method('hasHeader')->will($this->returnValue(true));
+        $this->request->expects($this->any())->method('getHeader')->will(
+            $this->onConsecutiveCalls('application/json', 'application/xml', 'text/xml', 'text/html' )
+        );
 
-        $m->expects($this->any())
-            ->method('hasHeader')
-            ->will($this->returnValue(true));
-
-        $m->expects($this->any())
-            ->method('getHeader')
-            ->will($this->onConsecutiveCalls('application/json', 'application/xml', 'text/xml', 'text/html' ));
-
-        $this->assertEquals('json', $this->server->getFormatFromHttpAccept($m));
-        $this->assertEquals('xml', $this->server->getFormatFromHttpAccept($m));
-        $this->assertEquals('xml', $this->server->getFormatFromHttpAccept($m));
-        $this->assertEquals(false, $this->server->getFormatFromHttpAccept($m));
-
+        $this->assertEquals('json', $this->server->getFormatFromHttpAccept($this->request));
+        $this->assertEquals('xml', $this->server->getFormatFromHttpAccept($this->request));
+        $this->assertEquals('xml', $this->server->getFormatFromHttpAccept($this->request));
+        $this->assertEquals(false, $this->server->getFormatFromHttpAccept($this->request));
     }
 
     /**
@@ -84,43 +64,133 @@ class ServerTest extends \PHPUnit_Framework_TestCase
     public function testNegotiateFormatUseDefaultAndthrowsException()
     {
         $c = array(
-            'default'        => 'defaultExt',
-            'controller_ext' => false,
-            'override'       => false,
-            'http_accept'    => false,
+            'default_format'    => 'defaultExt',
+            'controller_ext'    => false,
+            'format_override'   => false,
+            'http_accept'       => false,
         );
         $format = $this->server->negotiateFormat($c);
         $this->assertEquals('defaultExt', $format, "should get default format when all in the negotation chain are set to false");
     }
 
-    public function offtestNegotiateFormatUse()
+    public function negotiateFormatProvider()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
+        return array(
+            'controller_ext set to true' => array(
+                'uri'=>'/index.php/api/v1/mock.xml/test/param',
+                'options' => array(
+                    'route_prefix'      => '@^(/index.php)?/api/v(\d*)@i', // regex
+                    'default_format'    => 'xml',
+                    'controller_ext'    => true,
+                    'format_override'   => false,
+                    'http_accept'       => false,
+                ),
+               'expected' => array(
+                    'path' => '/mock/test/param',
+                    'format' => 'xml',
+                )
+            ),
+            'controller_ext set to false' => array(
+                'uri'=>'/index.php/api/v1/mock.json/test/param',
+                'options' => array(
+                    'route_prefix'      => '@^(/index.php)?/api/v(\d*)@i', // regex
+                    'default_format'    => 'json',
+                    'controller_ext'    => false,
+                    'format_override'   => false,
+                    'http_accept'       => false, // true or false
+                ),
+               'expected' => array(
+                    'path' => '/mock.json/test/param',
+                    'format' => 'json',
+                )
+            ),
+            'format_override set' => array(
+                'uri'=>'/index.php/api/v1/mock.json/test/param',
+                'options' => array(
+                    'route_prefix'      => '@^(/index.php)?/api/v(\d*)@i', // regex
+                    'default_format'    => 'json',
+                    'controller_ext'    => false,
+                    'format_override'   => 'html',
+                    'http_accept'       => false,
+                ),
+               'expected' => array(
+                    'path' => '/mock.json/test/param',
+                    'format' => 'html',
+                )
+            ),
+            'http_accept is set (but none provided)' => array(
+                'uri'=>'/index.php/api/v1/mock.json/test/param',
+                'options' => array(
+                    'route_prefix'      => '@^(/index.php)?/api/v(\d*)@i', // regex
+                    'default_format'    => 'xml',
+                    'controller_ext'    => false,
+                    'format_override'   => false,
+                    'http_accept'       => true,
+                ),
+               'expected' => array(
+                    'path' => '/mock.json/test/param',
+                    'format' => 'xml',
+                )
+            ),
+            'all false, shodu use default' => array(
+                'uri'=>'/index.php/api/v1/mock.json/test/param',
+                'options' => array(
+                    'route_prefix'      => '@^(/index.php)?/api/v(\d*)@i', // regex
+                    'default_format'    => 'xml',
+                    'controller_ext'    => false,
+                    'format_override'   => false,
+                    'http_accept'       => false,
+                ),
+               'expected' => array(
+                    'path' => '/mock.json/test/param',
+                    'format' => 'xml',
+                )
+            )
         );
-
-        $this->server->setRouting();
-
-        $m = $this->getMock('Zenya\Api\Server', array('extractExtension'));
-
-        $m->expects($this->any())
-            ->method('extractExtension')
-            ->will($this->returnValue(array('foo','bar')));
-
-        $c = array(
-            'default'        => 'json',
-            'controller_ext' => true,
-            'override'       => false, #isset($_REQUEST['format']) ? $_REQUEST['format'] : false,
-            'http_accept'    => false,
-        );
-
-        $format = $m->negotiateFormat($c);
-        $this->assertEquals('defaultExt', $format);
     }
 
-    // public function testGetResults()
-    // {
-    //     $this->assertSame(null, $this->server->getResults());
-    // }
+    /**
+     * @dataProvider negotiateFormatProvider
+     */
+    public function testSetRoutingNegotiateFormat($uri, array $options, array $expected)
+    {
+        $this->assertObjectHasAttribute('route', $this->server);
+
+        $this->request->expects($this->once())->method('getUri')->will($this->returnValue($uri));
+        $this->request->expects($this->once())->method('getMethod')->will($this->returnValue('GET'));
+
+        $this->server->setRouting($this->request, array('/whatever' => array()), $options);
+
+        $this->assertInstanceOf('Zenya\Api\Router', $this->server->route);
+        $this->assertSame($expected['path'], $this->server->route->path); // TODO: getter?
+
+        $this->assertInstanceOf('Zenya\Api\Response', $this->server->response);
+        $this->assertSame($expected['format'], $this->server->response->getFormat());
+    }
+
+    public function testSetRoutingWithHeader()
+    {
+        $uri = '/index.php/api/v1/mock/test/param';
+        $options = array(
+            'route_prefix'      => '@^(/index.php)?/api/v(\d*)@i', // regex
+            'default_format'    => 'xml',
+            'controller_ext'    => false,
+            'format_override'   => false,
+            'http_accept'       => true,
+        );
+
+        $this->request->expects($this->once())->method('getUri')->will($this->returnValue($uri));
+        $this->request->expects($this->once())->method('getMethod')->will($this->returnValue('GET'));
+
+        $this->request->expects($this->any())->method('hasHeader')->will($this->returnValue(true));
+        $this->request->expects($this->any())->method('getHeader')->will($this->returnValue('text/xml'));
+
+        $this->server->setRouting($this->request, array('/whatever' => array()), $options);
+
+        $this->assertSame('Accept', $this->server->response->getHeader('Vary'));
+        $this->assertSame('xml', $this->server->response->getFormat());
+    }
+
+    // TODO: resources and config parsinfg related!
 
 }
