1a4f015c993dc929096dc641a4811891d3f64a51
Franck Cassedanne <franck@ouarz.net>
2012-06-26 09:25:53 +0100

    Fixes on setRouteOverrides.

--- src/php/Zenya/Api/Config.php
+++ src/php/Zenya/Api/Config.php
@@ -201,7 +201,7 @@ class Config extends \Pimple
             'resources' => array(),
             'resources_default' => array(
                 // OPTIONS
-                'help' => array(
+                'onHelp' => array(
                     'controller' => array(
                         'name' => __NAMESPACE__ . '\Resource\Help',
                         'args' => null,
@@ -214,7 +214,7 @@ class Config extends \Pimple
                     ),
                 ),
                 // HEAD
-                'test' => array(
+                'onTest' => array(
                     'controller' => array(
                         'name' => __NAMESPACE__ . '\Resource\Test',
                         'args' => null #array( &$this ), #
