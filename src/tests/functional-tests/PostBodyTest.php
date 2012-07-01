52864ebc3e7a85a7e2e5db5fabf8ab71c9d57fb0
Franck Cassedanne <franck@ouarz.net>
2012-07-01 12:41:17 +0100

    REviewed functional test for PostBodyTest so it is generic and cross platform re XML implementation.

--- src/tests/functional-tests/PostBodyTest.php
+++ src/tests/functional-tests/PostBodyTest.php
@@ -3,13 +3,7 @@
 class PostBodyTest extends \PHPUnit_Framework_TestCase
 {
 
-    public $debug = null;
-
-    public function setUp()
-    {
-    }
-
-    public function post($body, $format=null, $url='http://sleepover.dev/index.php/api/v1/upload.json/keyword')
+    public function post($body, $format=null, $url='http://zenya.dev/index.php/api/v1/upload.json/keyword')
     {
         $cmd = "curl -s -X POST -d '${body}' '${url}'";
 
@@ -27,33 +21,58 @@ class PostBodyTest extends \PHPUnit_Framework_TestCase
     }
 
 
-    public function testBasic()
+    public function testFormPostAsString()
     {
         $r = $this->post('param1=value1&param2=value2');
 
         $this->assertEquals('param1=value1&param2=value2', $r->zenya->upload->body);
+    }
+
+   public function testFormPostAsArray()
+    {
+        $r = $this->post('param1=value1&param2=value2');
+
         $this->assertSame(array('param1'=>'value1', 'param2'=>'value2'), (array)$r->zenya->upload->params);
     }
 
-    public function testJson()
+    public function testJsonAsString()
     {
         $array = array('param1'=>'value1', 'param2'=>'value2');
         $r = $this->post(json_encode($array), 'json');
 
         $this->assertEquals('{"param1":"value1","param2":"value2"}', $r->zenya->upload->body);
-        $this->assertSame($array, (array)$r->zenya->upload->paramsBody);
     }
 
-    public function testXml()
+    public function testJsonAsArray()
+    {
+        $array = array('param1'=>'value1', 'param2'=>'value2');
+        $r = $this->post(json_encode($array), 'json');
+
+        $this->assertSame($array, (array)$r->zenya->upload->params);
+    }
+
+    public function testXmlAsString()
     {
         $array = array('param1'=>'value1', 'param2'=>'value2');
 
         $xml = new Zenya\Api\Output\Xml;
         $r = $this->post($xml->encode($array), 'xml');
 
-        $this->assertEquals('<?xml version="1.0" encoding="utf-8"?>'.PHP_EOL."<root>\n  <param1>value1</param1>\n  <param2>value2</param2>\n</root>",
-            $r->zenya->upload->body);
-        $this->assertSame($array, (array)$r->zenya->upload->paramsBody);
+        $this->assertEquals(
+            '<?xml version="1.0" encoding="utf-8"?><root><param1>value1</param1><param2>value2</param2></root>',
+            preg_replace('/[\r\n]+/', '', $r->zenya->upload->body)
+        );
     }
+
+   public function testXmlAsArray()
+    {
+        $array = array('param1'=>'value1', 'param2'=>'value2');
+
+        $xml = new Zenya\Api\Output\Xml;
+        $r = $this->post($xml->encode($array), 'xml');
+
+        $this->assertSame($array, (array)$r->zenya->upload->params);
+    }
+
 
 }
