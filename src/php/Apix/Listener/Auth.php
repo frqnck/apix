1d679ef709fb4f075b7b289e5f6c5f6e6de9d3fb
Franck Cassedanne <franck@ouarz.net>
2012-11-09 03:12:02 +0000

    Added unit test for CacheListener.

--- src/php/Apix/Listener/Auth.php
+++ src/php/Apix/Listener/Auth.php
@@ -1,56 +1,59 @@
 <?php
 namespace Apix\Listener;
 
-class Auth implements \SplObserver
+class Auth extends AbstractListener
 {
 
-    public $annotation = 'api_auth_role';
+    public $annotation = 'api_auth';
+
+    protected $options = array(
+        'public_group'  =>  'public'     // name of the public group to skip.
+    );
 
     /**
      * Constructor.
      *
-     * @param object $adapter The cache adapter.
-     *
-     * @throws \RuntimeException
+     * @param Cache\Adapter $adapter
+     * @param array $options Array of options.
      */
-    public function __construct($adapter=null)
+    public function __construct(Auth\Adapter $adapter, array $options=array())
     {
-
-        switch (true) {
-            case ($adapter instanceof Auth\Adapter):
-              $this->adapter = $adapter;
-            break;
-
-            case ($adapter instanceof \Zend_Auth):
-              echo "TODO: Zend_Auth";
-              exit;
-
-            default:
-                throw new \RuntimeException('Unable to open the Authentication adapter');
-        }
-
+        $this->adapter = $adapter;
+        $this->options = $options+$this->options;
     }
 
     public function update(\SplSubject $entity)
     {
-        $role = $entity->getAnnotationValue($this->annotation);
+        $this->entity = $entity;
 
-        // skip if null or public
-        if (is_null($role) || $role == 'public') {
-          return false;
+        $groups = $this->getSubTagValues('groups');
+        $users = $this->getSubTagValues('users');
+
+        // skip if groups and users are null, or public role group.
+        if( 
+            null === $groups || in_array($this->options['public_group'], $groups)
+            && null !== $users
+        ) {
+            return false;
         }
 
+        // authenticate
         if( ! $username = $this->adapter->authenticate() ) {
             throw new \Exception('Authentication required', 401);
+        }
+
+        // check user
+        if(null !== $users && !in_array($username, $users)) {
+            $this->log('Access unauthorised', $username);
+            throw new \Exception('Access unauthorised.', 401);
         }
 
         // todo set X_REMOTE_USER or X_AUTH_USER
         #$entity->getResponse()->setHeader('X_REMOTE_USER', $username);
         $_SERVER['X_AUTH_USER'] = $username;
 
+        #$this->log('Access granted', $username);
         return $username;
-
-
 
 
 
--- src/php/Apix/Listener/Cache.php
+++ src/php/Apix/Listener/Cache.php
@@ -1,49 +1,39 @@
 <?php
 namespace Apix\Listener;
 
-class Cache implements \SplObserver
+class Cache extends AbstractListener
 {
     protected $annotation = 'api_cache';
-    protected $adapter;
+        
     protected $options = array(
         'enable'    => true,        // Wether to enable caching at all.
         'ttl'       => '10mins',    // null stands forever.
         'flush'     => true,        // flush tags each time (e.g. cron job instead).
-        'tags'      => null,        // default tag(s) to append. TODO!
+        'tags'      => array('v1'), // default tag(s) to append. TODO!
     );
 
     /**
      * Constructor.
      *
-     * @param object $adapter
-     * @param array $events Array of events to listen to (default: all events)
-     *
-     * @return void
-     * @throws \RuntimeException
+     * @param Cache\Adapter $adapter
+     * @param array $options Array of options.
      */
-    public function __construct($adapter=null, array $options=array())
+    public function __construct(Cache\Adapter $adapter, array $options=array())
     {
+        $this->adapter = $adapter;
         $this->options = $options+$this->options;
-
-        switch (true) {
-            case ($adapter instanceof Cache\Adapter):
-                $this->adapter = $adapter;
-            break;
-
-            case ($adapter instanceof Zend_Cache):
-                $this->adapter = $adapter;
-            break;
-
-            default:
-              throw new \RuntimeException('Unable to open the Cache adapter');
-        }
     }
 
-    function log($msg, $ref=null)
+    /**
+     * Flush the tags explicitly
+     *
+     * @param boolean $enable Wether to flush or not.
+     */
+    public function flushAnnotatedTags($enable)
     {
-        if(defined('DEBUG')) {
-            $str = sprintf('%s %s (%s)', __CLASS__, $msg, $ref);
-            error_log( $str );
+        if ( $enable && $tags = $this->getSubTagValues('flush') ) {
+            $this->adapter->clean($tags);
+            $this->log('Tags purged', implode(', ', $tags));
         }
     }
 
@@ -55,14 +45,9 @@ class Cache implements \SplObserver
             || null === $entity->getAnnotationValue($this->annotation)){
             return false;
         }
-
         $this->entity = $entity;
 
-        // clean/purge tags explicitly
-        if ( $this->options['flush'] && $tags = $this->getTagValues('clean') ) {
-            $this->adapter->clean($tags);
-            $this->log('Tags purged', implode(', ', $tags));
-        }
+        $this->flushAnnotatedTags( $this->options['flush'] );
 
         // the cache id is simply the entity route name for now! todo
         $id = $entity->getRoute()->getPath();
@@ -70,7 +55,6 @@ class Cache implements \SplObserver
         // Use cache
         if ($cache = $this->adapter->load($id)) {
             $this->log('loading', $id);
-
             return $cache;
         }
 
@@ -81,59 +65,33 @@ class Cache implements \SplObserver
         );
 
         // ...and cache it.
-        $ttl = $this->getTagValues('ttl', $this->options['ttl']);
-        $ttl = $this->getTtl($ttl[0]);
+        $ttl = $this->getSubTagValues('ttl', array($this->options['ttl']));
+        $ttl = $this->getTtlInternval($ttl[0]);
 
-        $tags = $this->getTagValues('tags');
-        
+        $tags = array_merge(
+            $this->options['tags'],
+            $this->getSubTagValues('tags', array())
+        );
+        $tags = array_unique($tags);
+
         $this->adapter->save($data, $id, $tags, $ttl);
         $this->log(sprintf('saving for %d secs', $ttl), $id . ': ' . implode(', ', $tags));
         return $data;
     }
 
     /**
-     * Returns the TTL in seconds.
+     * Returns the time-to-live interval in seconds.
+     *
+     * @param string $until A date/time string. @see http://php.net/strtotime
+     * @param string $from A date/time string. @see http://php.net/strtotime
      * @return integer  The TTL in seconds.
      */
-    public function getTtl($until=null, $from='now')
+    public function getTtlInternval($until=null, $from='now')
     {
         return $until==0 ? null : strtotime($until)-strtotime($from);
         // $expiryDate = new \DateTime();
         // $interval = new \DateInterval($ttl);
         // return $expiryDate->add($interval);
-    }
-
-    /**
-     * Retuns the value of the specifified subtag.
-     *
-     * @param  string       $key        The $key to retrieve.
-     * @param  string|null  $default    The default value. 
-     * @return array|null               An indexed array of values or null.
-     */
-    public function getTagValues($key, $default=null)
-    {
-        $default= is_array($default)?$default:array($default); 
-        $tags = $this->extractTags();
-        $k=array_search($key, $tags['key']);
-        return false === $k ? $default : explode(',', $tags['value'][$k]);
-    }
-
-    /**
-     * Extracts all the subtags.
-     *
-     * @return array An associative array
-     */
-    public function extractTags($fresh=false)
-    {
-      static $matches = null;
-      if($matches === null || $fresh) {
-        $lines = $this->entity->getAnnotationValue($this->annotation);
-        if(!is_array($lines)) $lines = array($lines);
-        foreach($lines as $line) {
-          preg_match_all('/(?P<key>[^= ]+)=(?P<value>[^= ]+)/i', $line, $matches);
-        }
-      }
-      return $matches;
     }
 
 }
