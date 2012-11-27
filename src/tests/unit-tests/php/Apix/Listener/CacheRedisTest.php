6a04b96cebf060328fd81f5b01a971274fbbc7a4
Franck Cassedanne <franck@ouarz.net>
2012-11-27 14:26:27 +0000

    Added Redis cache.

--- src/tests/unit-tests/php/Apix/Listener/CacheRedisTest.php
+++ src/tests/unit-tests/php/Apix/Listener/CacheRedisTest.php
@@ -3,34 +3,42 @@ namespace Apix\Listener\Cache;
 
 use Apix\ApixTestCase;
 
-/*
-    php -d apc.enable_cli=1 `which phpunit` -v src/tests/unit-tests/php/Apix/Listener/CacheApcTest.php
- */
-class CacheApcTest extends ApixTestCase
+class CacheRedisTest extends ApixTestCase
 {
 
     protected $cache;
 
+    const HOST = '127.0.0.1';
+    const PORT = 6379;
+    const AUTH = NULL; //replace with a string to use Redis authentication
+
     public function setUp()
     {
-        if(!extension_loaded('apc')) {
+        if(!extension_loaded('redis')) {
             $this->markTestSkipped(
-                'The APC extension is required in order to run this unit test'
+                'The Redis extension is required in order to run this unit test.'
             );
         }
+        // version_compare($this->version, "2.4.0", "ge")
 
-        if(!ini_get('apc.enable_cli')) {
-            $this->markTestSkipped(
-                'apc.enable_cli MUST be enable in order to run this unit test'
-            );
+        $this->redis = new \Redis();
+        $this->redis->connect(self::HOST, self::PORT);
+        if(self::AUTH) {
+            $this->assertTrue($this->redis->auth(self::AUTH));
         }
 
-        $this->cache = new Apc;
+        $this->cache = new Redis(
+            $this->redis,
+            array(
+                'key_prefix' => 'unittest-apix-key:',
+                'tag_prefix' => 'unittest-apix-tag:'
+            )
+        );
     }
 
     protected function tearDown()
     {
-        apc_clear_cache('user');
+        $this->cache->flush();
         unset($this->cache);
     }
 
@@ -44,9 +52,6 @@ class CacheApcTest extends ApixTestCase
         $this->assertTrue( $this->cache->save('strData', 'id') );
 
         $this->assertEquals( 'strData', $this->cache->load('id') );
-
-        $id = $this->cache->mapKey('id');
-        $this->assertEquals( 'strData', apc_fetch($id) );
     }
 
     public function testSaveWithTags()
@@ -60,7 +65,7 @@ class CacheApcTest extends ApixTestCase
         );
 
         $ids = $this->cache->load('tag2', 'tag');
-         $this->assertEquals( array($this->cache->mapKey('id1')), $ids );
+        $this->assertEquals( array($this->cache->mapKey('id1')), $ids );
     }
 
     public function testSaveWithOverlappingTags()
@@ -82,39 +87,57 @@ class CacheApcTest extends ApixTestCase
     public function testClean()
     {
         $this->cache->save('strData1', 'id1', array('tag1', 'tag2'));
-        $this->cache->save('strData2', 'id2', array('tag2', 'tag3'));
+        $this->cache->save('strData2', 'id2', array('tag2', 'tag3', 'tag4'));
         $this->cache->save('strData3', 'id3', array('tag3', 'tag4'));
 
         $this->cache->clean(array('tag4'));
 
         $this->assertNull($this->cache->load('id3'));
         $this->assertNull($this->cache->load('tag4', 'tag'));
-        $this->assertEquals('strData2', $this->cache->load('id2'));
+        $this->assertEquals('strData1', $this->cache->load('id1'));
     }
 
-    public function testFlush()
+    public function testFlushSelected()
     {
-        apc_add('foo', 'bar');
         $this->cache->save('strData1', 'id1', array('tag1', 'tag2'));
         $this->cache->save('strData2', 'id2', array('tag2', 'tag3'));
         $this->cache->save('strData3', 'id3', array('tag3', 'tag4'));
 
+        $this->redis->set('test', 'testValue');
         $this->cache->flush();
+        $this->assertTrue($this->redis->exists('test'));
 
         $this->assertNull($this->cache->load('id3'));
         $this->assertNull($this->cache->load('tag1', 'tag'));
-        $this->assertEquals('bar', apc_fetch('foo'));
+    }
+
+    public function testFlush()
+    {
+        $this->cache->save('strData1', 'id1', array('tag1', 'tag2'));
+        $this->cache->save('strData2', 'id2', array('tag2', 'tag3'));
+        $this->cache->save('strData3', 'id3', array('tag3', 'tag4'));
+
+        $this->cache->flush(true);
+
+        $this->assertNull($this->cache->load('id3'));
+        $this->assertNull($this->cache->load('tag1', 'tag'));
     }
 
     public function testDelete()
     {
-        $this->cache->save('strData1', 'id1', array('tag1', 'tag2'));
+        $this->cache->save('strData1', 'id1', array('tag1', 'tag2', 'tagz'));
         $this->cache->save('strData2', 'id2', array('tag2', 'tag3'));
 
         $this->cache->delete('id1');
 
         $this->assertNull($this->cache->load('id1'));
         $this->assertNull($this->cache->load('tag1', 'tag'));
+        $this->assertNull($this->cache->load('tagz', 'tag'));
+
+        $this->assertContains(
+            $this->cache->mapKey('id2'),
+            $this->cache->load('tag2', 'tag')
+        );
     }
 
     public function testDeleteInexistant()
@@ -122,35 +145,28 @@ class CacheApcTest extends ApixTestCase
         $this->assertFalse($this->cache->delete('Inexistant'));
     }
 
-    /**
-     * -runTestsInSeparateProcesses
-     * -runInSeparateProcesses
-     * -preserveGlobalState enable
-     * -depends test
-     */
     public function testShortTtlDoesExpunge()
     {
-        $this->markTestSkipped(
-            "APC will only expunged its cache on the next request which makes "
-            . "this specific unit untestable!?... :-("
-        );
         $this->cache->save('ttl-1', 'ttlId', null, -1);
-        // $this->assertSame('ttl-1', apc_fetch($this->cache->mapKey('ttlId')));
-        $this->assertNull( $this->cache->load('ttlId'), "Shoudl be null!!!!!!");
+
+        // $this->cache->save('ttl-1', 'ttlId', null, 1);
+        // sleep(1);
+
+        $this->assertNull( $this->cache->load('ttlId'), "Should be null");
     }
 
-    public function testGetInternalInfos()
-    {
-        $this->cache->save('someData', 'someId', null, 69);
-        $infos = $this->cache->getInternalInfos('someId');
-        $this->assertSame(69, $infos['ttl']);
-    }
+    // public function testGetInternalInfos()
+    // {
+    //     $this->cache->save('someData', 'someId', null, 69);
+    //     $infos = $this->cache->getInternalInfos('someId');
+    //     $this->assertSame(69, $infos['ttl']);
+    // }
 
-    public function testGetInternalInfosReturnFalseWhenNonExistant()
-    {
-        $this->assertFalse(
-            $this->cache->getInternalInfos('non-existant')
-        );
-    }
+    // public function testGetInternalInfosReturnFalseWhenNonExistant()
+    // {
+    //     $this->assertFalse(
+    //         $this->cache->getInternalInfos('non-existant')
+    //     );
+    // }
 
 }
