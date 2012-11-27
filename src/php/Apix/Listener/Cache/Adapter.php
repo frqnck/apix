7637ab81c20770ef4cd2798d9a730db99fc1a88c
Franck Cassedanne <franck@ouarz.net>
2012-11-27 14:26:27 +0000

    Added Redis cache.

--- src/php/Apix/Listener/Cache/Adapter.php
+++ src/php/Apix/Listener/Cache/Adapter.php
@@ -5,38 +5,38 @@ interface Adapter
 {
 
     /**
-     * Retrieves the cache for the given id, or return false if not set.
+     * Retrieves the cache for the given key, or return false if not set.
      *
-     * @param  string  $id      The cache id.
+     * @param  string  $key     The cache id to retrieve.
      * @return string|false     Returns the cached data.
      */
-    public function load($id);
+    public function load($key);
 
     /**
      * Saves data to the cache.
      *
      * @param  string   $data   The data to cache.
-     * @param  string   $id     The cache id.
+     * @param  string   $key    The cache id to save.
      * @param  array    $tags   The cache tags for this entry.
      * @param  int      $ttl    The time to live in seconds, if set to null the
      *                          cache is valid forever.
-     * @return boolean  True on sucess.
+     * @return boolean          True on sucess.
      */
-    public function save($data, $id, array $tags=null, $ttl=false);
+    public function save($data, $key, array $tags=null, $ttl=false);
 
     /**
      * Removes all the cached items associated with the given tag names.
      *
-     * @param  array  $tags The array of tags to remove.
+     * @param  array  $tags     The array of tags to remove.
      */
-    public function clean(array $tags=null);
+    public function clean(array $tags);
 
     /**
      * Deletes the specified cache record.
      *
-     * @param  string $id The cache id to remove.
-     * @return boolean True on sucess.
+     * @param  string $key      The cache id to remove.
+     * @return boolean          True on sucess.
      */
-    public function delete($id);
+    public function delete($key);
 
 }
