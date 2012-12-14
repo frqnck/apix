d93e4d9e4ecad7c125157395d1a5c1c8356347ac
Franck Cassedanne <franck@ouarz.net>
2012-12-14 18:40:22 +0000

    Names of few options in Plugin/Cache renamed.

--- src/php/Apix/Plugin/Auth/Adapter.php
+++ src/php/Apix/Plugin/Auth/Adapter.php
@@ -7,7 +7,6 @@ interface Adapter
     /**
      * Performs an authentication attempt
      *
-     * @throws Zend_Auth_Adapter_Exception If authentication cannot be performed
      * @return boolean
      */
     public function authenticate();
