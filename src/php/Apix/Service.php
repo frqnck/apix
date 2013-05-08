92b2430c99911e135cd42e89dbc5d5c39b7edf9d
Franck Cassedanne <franck@ouarz.net>
2013-05-08 17:35:38 +0100

    Whitespace.

--- src/php/Apix/Service.php
+++ src/php/Apix/Service.php
@@ -8,7 +8,7 @@ class Service
     /**
      * Returns the specified service -- or all if unspecified.
      *
-     * @param  string $key=null The service key to retrieve.
+     * @param  string     $key=null  The service key to retrieve.
      * @param  array|null $args=null An array of argument to pass to the service.
      * @return mixed
      */
