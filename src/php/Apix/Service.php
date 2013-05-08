a098965c1ee8fbab91e41368bf9c58335ea77297
Franck Cassedanne <franck@ouarz.net>
2013-05-08 17:26:06 +0100

    Many chnages pertient to Auth, Added a Session object.

--- src/php/Apix/Service.php
+++ src/php/Apix/Service.php
@@ -9,11 +9,12 @@ class Service
      * Returns the specified service -- or all if unspecified.
      *
      * @param  string $key=null The service key to retrieve.
+     * @param  array|null $args=null An array of argument to pass to the service.
      * @return mixed
      */
-    public static function get($key=null)
+    public static function get($key=null, $args=null)
     {
-        return Config::getInstance()->getServices($key);
+        return Config::getInstance()->getServices($key, $args);
     }
 
     /**
