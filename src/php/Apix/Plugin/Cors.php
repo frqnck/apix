9b3515aeefcd4db41c01e3e61ec58c4af5270b89
Franck Cassedanne <franck@ouarz.net>
2014-09-04 16:43:25 +0100

    Finnished CORS implementation and unit-tests.

--- src/php/Apix/Plugin/Cors.php
+++ src/php/Apix/Plugin/Cors.php
@@ -20,6 +20,7 @@ use Apix\Service,
  * Apix plugin providing Cross-Origin Resource Sharing
  *
  * @see http://www.w3.org/TR/cors/
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
  */
 class Cors extends PluginAbstractEntity
 {
@@ -28,24 +29,27 @@ class Cors extends PluginAbstractEntity
     protected $annotation = 'api_cors';
 
     protected $options = array(
-        'enable'    => true,            // enable by default or not
+        'enable'    => true,            // whether to enable or not
 
         // -- whitelist (regex)
         'scheme'    => 'https?',        // allows both http and https
-        'host'      => '.*\.info\.com', // the host domain(s) or ip(s) to allow
-        'port'      => '(:[0-9]+)?',    // the port(s) allowed
+        'host'      => '.*\.info\.com', // the allowed host domain(s) or ip(s)
+        'port'      => '(:[0-9]+)?',    // the alowed port(s)
 
         // -- CORS directives
-        'allow-origin'      => 'origin', // 'origin', '*', or string-list or null
-        'allow-credentials' => false,
-        'expose-headers'    => null,
+        'allow-origin'      => 'origin', // 'origin', '*', 'null', string-list
+        'allow-credentials' => false,    // wether to allow Cookies and HTTP Auth
+        'expose-headers'    => null,     // comma-delimited HTTP headers exposed
 
         // -- preflight
-        'max-ages'      => 3600,
-        'allow-methods' => 'GET, POST',
-        'allow-headers' => 'some',
+        'max-age'       => 3600,         // TTL in seconds for preflight
+        'allow-methods' => 'GET,POST',   // comma-delimited HTTP methods allowed 
+        'allow-headers' => 'x-apix',     // comma-delimited HTTP headers allowed
     );
 
+    /**
+     * @{@inheritdoc}
+     */
     public function update(\SplSubject $entity)
     {
         $this->setEntity($entity);
@@ -70,7 +74,7 @@ class Cors extends PluginAbstractEntity
         if (
             $http_origin
             // && $_SERVER['HTTP_HOST'] == $http_origin
-            && $this->isOriginAllowed(
+            && self::isOriginAllowed(
                 $http_origin,
                 $this->options['host'],
                 $this->options['port'], $this->options['scheme']
@@ -86,9 +90,10 @@ class Cors extends PluginAbstractEntity
 
             // 5.2 Access-Control-Allow-Credentials
             // The actual request can include user credentials
-            // (e.g. cookies, XmlHttpRequest.withCredentials=true)
-            $response->setHeader('Access-Control-Allow-Credentials',
-                                    (bool) $this->options['allow-credentials']);
+            // e.g. cookies, XmlHttpRequest.withCredentials=true
+            if ($this->options['allow-credentials'] === true) {
+                $response->setHeader('Access-Control-Allow-Credentials', true);
+            }
 
             // 5.3 Access-Control-Expose-Headers
             // Which response headers are available (besides the generic ones)
@@ -97,28 +102,74 @@ class Cors extends PluginAbstractEntity
                                             $this->options['expose-headers']);
             }
 
-            // TODO: Preflight...
-            $preflight = false;
-            if ($preflight) {
+            $request = $response->getRequest();
+            if ( self::isPreflight($request) ) {
+                
                 // 5.4 Access-Control-Max-Age
-                // Cache the preflight request for the provided amount of seconds.
-                if ($this->options['max-ages'] > 0) {
+                if ($this->options['max-age'] > 0) {
+                    // Cache the request for the provided amount of seconds
                     $response->setHeader('Access-Control-Max-Age',
-                                            (int) $this->options['max-ages']);
+                                            (int) $this->options['max-age']);
                 }
 
                 // 5.5 Access-Control-Allow-Methods
-                $response->setHeader('Access-Control-Allow-Methods',
-                                            $this->options['allow-methods']);
+                if ($request->hasHeader('Access-Control-Request-Method') ) {
+                   if (!in_array(
+                        $request->getHeader('Access-Control-Request-Method'), 
+                        self::split($this->options['allow-methods'])
+                    )) {
+                        return self::exception(); 
+                    }
+                    $response->setHeader(
+                        'Access-Control-Allow-Methods',
+                        $this->options['allow-methods']
+                    );
+                }
 
                 // 5.6 Access-Control-Allow-Headers
-                $response->setHeader('Access-Control-Allow-Headers',
-                                            $this->options['allow-headers']);
+                if ($request->hasHeader('Access-Control-Request-Headers')) {
+                    $req_headers = self::split(
+                        $request->getHeader('Access-Control-Request-Headers')
+                    );
+                    $allowed = self::split($this->options['allow-headers']);
+                    foreach($req_headers as $req_header) {
+                        if (!in_array($req_header, $allowed)) {
+                            return self::exception(); 
+                        }
+                    }
+                    $response->setHeader(
+                        'Access-Control-Allow-Headers',
+                        $this->options['allow-headers']
+                    );
+                }
             }
 
-        } else {
-            throw new Exception('Not a valid CORS request.', 400);
+            return true;
         }
+
+        // so it must be an invalid CORS request 
+        return self::exception();
+    }
+
+    /**
+     * Throws a \DomainException.
+     *
+     * @throw \DomainException
+     */
+    public static function exception()
+    {
+        throw new \DomainException('Not a valid CORS request.', 403);
+    }
+
+    /**
+     * Split and trim the provided string.
+     *
+     * @param  string  $str
+     * @return array
+     */
+    public static function split($str)
+    {
+        return array_map('trim', explode(',', $str)); 
     }
 
     /**
@@ -129,12 +180,41 @@ class Cors extends PluginAbstractEntity
      * @param  string  $scheme Default tp 'http' and 'https'.
      * @return boolean
      */
-    public function isOriginAllowed(
+    public static function isOriginAllowed(
         $origin, $host, $port='(:[0-9]+)?', $scheme='https?'
     ) {
         $regex = '`^' . $scheme . ':\/\/' . $host . $port . '$`';
 
         return (bool) preg_match($regex, $origin);
+    }
+
+
+    /**
+     * Checks for a preflighted request.
+     *
+     * @return boolean
+     */
+    public static function isPreflight(HttpRequest $request)
+    {
+        $method = $request->getMethod();
+        return 
+            // uses methods other than...
+            !in_array($method, array('GET', 'HEAD', 'POST'))
+
+            // if POST is used with a Content-Type other than...
+            or ( $method == 'POST'
+                 and !in_array(
+                        $request->getHeader('CONTENT_TYPE'),
+                        array(
+                            'text/plain',
+                            'multipart/form-data',
+                            'application/x-www-form-urlencoded'
+                        )
+                    )
+                )
+            
+            // if it is set with some custom request headers
+            or $request->hasHeader('Access-Control-Request-Headers'); 
     }
 
 }
--- src/tests/unit-tests/php/Apix/Plugins/CorsTest.php
+++ src/tests/unit-tests/php/Apix/Plugins/CorsTest.php
@@ -14,18 +14,23 @@ namespace Apix\Plugin;
 
 use Apix\HttpRequest,
     Apix\Response,
-    Apix\TestCase;
+    Apix\TestCase,
+    Apix\Service;
 
 class CorsTest extends TestCase
 {
 
-    protected $plugin, $response;
+    protected $plugin, $request, $response, $route, $opts;
 
     public function setUp()
     {
-        $this->request = HttpRequest::GetInstance();
+        $_SERVER['HTTP_ORIGIN'] = "https://foo.bar";
+
+        $this->request = new HttpRequest();
         $this->response = new Response($this->request);
         $this->response->unit_test = true;
+
+        Service::set('response', $this->response);
 
         $this->route = $this->getMock('Apix\Router');
         $this->entity = $this->getMock('Apix\Entity');
@@ -40,18 +45,104 @@ class CorsTest extends TestCase
 
         $this->response->setRoute($this->route);
 
-        $this->plugin = new Cors();
+        $this->plugin = new Cors( array('enable' => true, 'host' => 'foo\.bar') );
+
+        $this->opts = $this->plugin->getOptions();
     }
 
     protected function tearDown()
     {
-        unset($this->plugin, $this->response, $this->route);
+        unset($this->plugin, $this->request, $this->response, $this->route, $this->opts);
     }
 
     public function testIsDisable()
     {
-        $plugin = new Cors( array('enable' => false) );
-        $this->assertFalse( $plugin->update( $this->entity ) );
+        $this->plugin = new Cors( array('enable' => false) );
+        $this->assertFalse( $this->plugin->update( $this->entity ) );
+    }
+
+    protected function updatePlugin()
+    {
+        $this->plugin = new Cors( $this->opts );
+        $this->assertTrue( $this->plugin->update( $this->entity ) );
+        // return $plugin;
+    }
+
+    /**
+     * @expectedException           \DomainException
+     * @expectedExceptionCode       403
+     */
+    public function testNotAllowedThrowsDomainException()
+    {
+        $_SERVER['HTTP_ORIGIN'] = null;
+        $this->updatePlugin();
+    }
+
+    public function testAllowOriginIsOrigin()
+    {
+        $this->opts['allow-origin'] = 'origin';
+        $this->updatePlugin();
+        $this->assertSame(
+            $_SERVER['HTTP_ORIGIN'],
+            $this->response->getHeader('Access-Control-Allow-Origin')
+        );
+    }
+
+    public function testAllowOriginIsStringList()
+    {
+        $this->opts['allow-origin'] = 'foo,bar';
+        $this->updatePlugin();
+        $this->assertSame(
+            $this->opts['allow-origin'],
+            $this->response->getHeader('Access-Control-Allow-Origin')
+        );
+    }
+
+    public function testAllowOriginIsNull()
+    {
+        $this->opts['allow-origin'] = 'null';
+        $this->updatePlugin();
+        $this->assertSame(
+            $this->opts['allow-origin'],
+            $this->response->getHeader('Access-Control-Allow-Origin')
+        );
+    }
+
+    public function testAllowCredentials()
+    {
+        $this->opts['allow-credentials'] = true;
+        $this->updatePlugin();
+        $this->assertTrue(
+            $this->response->getHeader('Access-Control-Allow-Credentials')
+        );
+    }
+    
+    public function testAllowCredentialsSetToFalse()
+    {
+        $this->opts['allow-credentials'] = false;
+        $this->updatePlugin();
+        $this->assertNull(
+            $this->response->getHeader('Access-Control-Allow-Credentials')
+        );
+    }
+
+    public function testExposeHeadersIsNull()
+    {
+        $this->opts['expose-headers'] = null;
+        $this->updatePlugin();
+        $this->assertNull(
+            $this->response->getHeader('Access-Control-Expose-Headers')
+        );
+    }
+
+    public function testExposeHeadersIsSet()
+    {
+        $this->opts['expose-headers'] = 'x-foo-bar';
+        $this->updatePlugin();
+        $this->assertSame(
+            $this->opts['expose-headers'],
+            $this->response->getHeader('Access-Control-Expose-Headers')
+        );
     }
 
     /**
@@ -62,11 +153,11 @@ class CorsTest extends TestCase
     ) {
         $this->assertSame(
             $result,
-            $this->plugin->isOriginAllowed($origin, $host, $port, $scheme)
+            Cors::isOriginAllowed($origin, $host, $port, $scheme)
         );
     }
 
-    public function OriginsProvider()
+    public function originsProvider()
     {
         return array(
             array(
@@ -136,18 +227,101 @@ class CorsTest extends TestCase
         );
     }
 
-    public function TODOtestHearder()
+    public function testIsPreflightIsFalse()
     {
-        $this->plugin->update($this->response);
+        $this->request->setMethod('GET');
+        $this->assertFalse( Cors::IsPreflight($this->request) );
 
-        var_dump( $this->response->getHeaders() );
+        $this->request->setMethod('HEAD');
+        $this->assertFalse( Cors::IsPreflight($this->request) );
 
-        var_dump( $this->response->getHeaders() );
+        $this->request->setHeader('CONTENT_TYPE', 'text/plain');
+        $this->request->setMethod('POST');
+        $this->assertFalse( Cors::IsPreflight($this->request) );
 
-        $this->assertSame(
-            array(),
-            $this->response->getHeaders()
+        $this->assertNull(
+            $this->response->getHeader('Access-Control-Max-Age')
         );
+    }
+
+    public function testIsPreflightIsTrue()
+    {
+        $this->request->setMethod('OPTIONS');
+        $this->assertTrue( Cors::IsPreflight($this->request) );
+
+        $this->request->setHeader('CONTENT_TYPE', 'text/xml');
+        $this->request->setMethod('POST');
+        $this->assertTrue( Cors::IsPreflight($this->request) );
+    }
+
+    public function testIfPreflightDoesSetAccessControlMaxAge()
+    {
+        $this->request->setMethod('OPTIONS');
+        $this->updatePlugin();
+        $this->assertEquals(
+            $this->opts['max-age'],
+            $this->response->getHeader('Access-Control-Max-Age')
+        );
+    }
+
+    public function testIfPreflightWithRequestMethod()
+    {
+        $this->opts['allow-methods'] = 'FOO, BAR';
+
+        $this->request->setMethod('OPTIONS');
+        $this->request->setHeader('Access-Control-Request-Method', 'BAR');
+        $this->assertTrue( Cors::IsPreflight($this->request) );
+
+        $this->updatePlugin();
+
+        $this->assertEquals(
+            $this->opts['allow-methods'],
+            $this->response->getHeader('Access-Control-Allow-Methods')
+        );
+    }
+
+    /**
+     * @expectedException           \DomainException
+     * @expectedExceptionCode       403
+     */
+    public function testIfPreflightWithWrongRequestMethod()
+    {
+        $this->opts['allow-methods'] = 'FOO, BAR';
+
+        $this->request->setMethod('OPTIONS');
+        $this->request->setHeader('Access-Control-Request-Method', 'PUT');
+        $this->assertTrue( Cors::IsPreflight($this->request) );
+
+        $this->updatePlugin();
+    }
+
+    public function testIfPreflightWithRequestHeaders()
+    {
+        $this->opts['allow-headers'] = 'x-foo-bar,x-booz-bar';
+
+        $this->request->setHeader('Access-Control-Request-Headers', 'x-booz-bar');
+        $this->assertTrue( Cors::IsPreflight($this->request) );
+
+        $this->updatePlugin();
+
+        $this->assertEquals(
+            $this->opts['allow-headers'],
+            $this->response->getHeader('Access-Control-Allow-Headers')
+        );
+    }
+
+    /**
+     * @expectedException           \DomainException
+     * @expectedExceptionCode       403
+     */
+    public function testIfPreflightWithWrongRequestHeaders()
+    {
+        $this->opts['allow-headers'] = 'x-foo-bar,x-booz-bar';
+
+        $this->request->setHeader('Access-Control-Request-Headers', 'x-null');
+        $this->assertTrue( Cors::IsPreflight($this->request) );
+
+        $this->updatePlugin();
     }
 
 }
