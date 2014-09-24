5a180bf2ee9434512831deb6009060a8db959be1
Franck Cassedanne <franck@ouarz.net>
2014-09-24 12:23:55 +0100

    Added new tests.

---
+++ src/tests/unit-tests/php/Apix/Plugins/AuthTest.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ *
+ * This file is part of the Apix Project.
+ *
+ * (c) Franck Cassedanne <franck at ouarz.net>
+ *
+ * @license     http://opensource.org/licenses/BSD-3-Clause  New BSD License
+ *
+ */
+
+namespace Apix\Plugin;
+
+use Apix\HttpRequest,
+    Apix\Response,
+    Apix\TestCase,
+    Apix\Service;
+
+class AuthTest extends TestCase
+{
+    protected $plugin, $request, $response, $opts, $authAdapter;
+
+    public function setUp()
+    {
+        $this->request = new HttpRequest();
+        $this->response = new Response($this->request);
+        $this->response->unit_test = true;
+
+        Service::set('response', $this->response);
+
+        $this->entity = $this->getMock('Apix\Entity');
+
+        $this->authAdapter = $this->getMock('Apix\Plugin\Auth\Adapter');
+        $this->plugin = new Auth( array('adapter' => $this->authAdapter) );
+
+        $this->opts = $this->plugin->getOptions();
+    }
+
+    protected function setEntityAnnotation($key, $value)
+    {
+        $this->entity
+            ->expects($this->any())
+            ->method('getAnnotationValue')
+            ->with($this->equalTo($key))
+            ->will($this->returnValue((string) $value));
+    }
+
+    protected function tearDown()
+    {
+        unset(
+            $this->plugin, $this->request, $this->response, $this->opts,
+            $this->authAdapter
+        );
+    }
+
+    public function testIsDisableWhenBotGroupAndUserAreNotSet()
+    {
+        $this->assertNull( $this->plugin->update( $this->entity ) );
+    }
+
+    public function testIsDisableIfPublicGroup()
+    {
+        $this->setEntityAnnotation('api_auth', 'groups=' . $this->opts['public_group']);
+        $this->assertNull( $this->plugin->update( $this->entity ) );
+    }
+
+    /**
+     * @expectedException           \Apix\Exception
+     * @expectedExceptionCode       401
+     * @expectedExceptionMessage    Authentication required
+     */
+    public function testTryToAuthWhenUserIsSet()
+    {
+        $this->setEntityAnnotation('api_auth', 'users=frqnck');
+        $this->plugin->update( $this->entity );
+    }
+
+    /**
+     * @expectedException           \Apix\Exception
+     * @expectedExceptionCode       401
+     * @expectedExceptionMessage    Authentication required
+     */
+    public function testTryToAuthWhenGroupIsSet()
+    {
+        $this->setEntityAnnotation('api_auth', 'groups=admin');
+        $this->plugin->update( $this->entity );
+    }
+
+    public function testAuthIsSuccessful()
+    {
+        Service::set('session', null);
+
+        // $_SERVER['PHP_AUTH_USER'] = 'frqnck';
+
+        $this->authAdapter->expects($this->once())
+            ->method('authenticate')
+            ->will($this->returnValue(true));
+
+        $this->setEntityAnnotation('api_auth', 'users=frqnck');
+        $this->assertTrue( $this->plugin->update( $this->entity ) );
+    }
+
+    // -------- TEMP Session 
+
+    /**
+     * @expectedException           \Apix\Exception
+     * @expectedExceptionCode       401
+     * @expectedExceptionMessage    Access unauthorised
+     * @group session
+     */
+    public function testSessionWrongUser()
+    {
+        Service::set('session', new \Apix\Session('some_user'));
+
+        $this->authAdapter->expects($this->once())
+            ->method('authenticate')
+            ->will($this->returnValue(true));
+
+        $this->setEntityAnnotation('api_auth', 'users=frqnck');
+        $this->plugin->update( $this->entity );
+    }
+
+    /**
+     * @expectedException           \Apix\Exception
+     * @expectedExceptionCode       401
+     * @expectedExceptionMessage    Access unauthorised.
+     * @group session
+     */
+    public function testSessionWrongGroup()
+    {
+        Service::set('session', new \Apix\Session('frqnck', 'some_group'));
+
+        $this->authAdapter->expects($this->once())
+            ->method('authenticate')
+            ->will($this->returnValue(true));
+
+        $this->setEntityAnnotation('api_auth', 'users=frqnck groups=admin');
+        $this->plugin->update( $this->entity );
+    }
+
+    /**
+     * @expectedException           \Apix\Exception
+     * @expectedExceptionCode       401
+     * @expectedExceptionMessage    Session's IP not trusted
+     * @group session
+     */
+    public function testSessionIpIsNotTrusted()
+    {
+        Service::set('session', new \Apix\Session('frqnck', 'admin'));
+        Service::get('session')->setTrustedIps((array) '1.2.3.4');
+        
+        $this->authAdapter->expects($this->once())
+            ->method('authenticate')
+            ->will($this->returnValue(true));
+
+        $this->setEntityAnnotation('api_auth', 'users=frqnck groups=admin');
+        $this->plugin->update( $this->entity );
+    }
+
+    /**
+     * @group session
+     */
+    public function testSessionIpIsTrusted()
+    {
+        Service::get('response')->getRequest()->setHeader(
+            'HTTP_CLIENT_IP', '1.2.3.4'
+        );
+
+        Service::set('session', new \Apix\Session('frqnck', 'admin'));
+        Service::get('session')->setTrustedIps((array) '1.2.3.4');
+        
+        $this->authAdapter->expects($this->once())
+            ->method('authenticate')
+            ->will($this->returnValue(true));
+
+        $this->setEntityAnnotation('api_auth', 'groups=admin');
+        $this->assertTrue( $this->plugin->update( $this->entity ) );
+        $this->assertSame('frqnck', $_SERVER['X_AUTH_USER']);
+    }
+
+}
---
+++ src/tests/unit-tests/php/Apix/Plugins/CacheTest.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *
+ * This file is part of the Apix Project.
+ *
+ * (c) Franck Cassedanne <franck at ouarz.net>
+ *
+ * @license     http://opensource.org/licenses/BSD-3-Clause  New BSD License
+ *
+ */
+
+namespace Apix\Plugin;
+
+use Apix\HttpRequest,
+    Apix\Response,
+    Apix\TestCase,
+    Apix\Service;
+
+class CacheTest extends TestCase
+{
+    protected $plugin, $request, $response, $opts;
+
+    public function setUp()
+    {
+        $this->request = new HttpRequest();
+        $this->response = new Response($this->request);
+        $this->response->unit_test = true;
+
+        Service::set('response', $this->response);
+
+        $this->entity = $this->getMock('Apix\Entity');
+
+        $this->plugin = new Cache( array('enable' => true, 'adapter' => 'Apix\Cache\Runtime') );
+
+        $this->opts = $this->plugin->getOptions();
+    }
+
+    protected function tearDown()
+    {
+        unset($this->plugin, $this->request, $this->response, $this->opts);
+    }
+
+    public function testIsDisable()
+    {
+        $this->opts['enable'] = false;
+        $plugin = new Cache( $this->opts );
+        $this->assertFalse( $plugin->update( $this->entity ) );
+    }
+    
+    public function testFlushAnnotatedTagsReturnsNull()
+    {
+        $this->plugin->update( $this->entity );
+
+        $this->assertNull( $this->plugin->flushAnnotatedTags(false) );
+        $this->assertNull( $this->plugin->flushAnnotatedTags(true) );
+    }
+
+    public function testFlushAnnotatedTagsReturnFalse()
+    {
+        $this->plugin->update( $this->entity );
+        $tags = array('tag1');
+
+        $this->assertFalse( $this->plugin->flushAnnotatedTags(true, $tags) );
+    }
+
+    public function testFlushAnnotatedTagsReturnTrue()
+    {
+        $this->plugin->update( $this->entity );
+        $tags = array('tag1');
+
+        // save the tags and flush them.
+        $this->plugin->getAdapter()->save('foo value', 'foo', $tags);
+        $this->assertTrue( $this->plugin->flushAnnotatedTags(true, $tags) );
+    }
+
+    public function testReturnFreshData()
+    {
+        $val = 'fresh method value';
+        $this->entity->expects($this->any())
+                ->method('call')
+                ->will($this->returnValue($val));
+
+        $data = $this->plugin->update($this->entity);
+        $this->assertSame($val, $data);
+    }
+
+    public function testReturnCachedData()
+    {
+        $_SERVER['HTTP_X_REWRITE_URL'] = '/foo';
+        $this->assertSame($_SERVER['HTTP_X_REWRITE_URL'], $this->request->getRequestUri());
+
+        $this->plugin->getAdapter()->save('foo value from cache', '/foo');
+
+        $data = $this->plugin->update($this->entity);
+        $this->assertSame('foo value from cache', $data);
+    }
+
+    /**
+     * @expectedException           \Exception
+     */
+    public function testReThrowsException()
+    {
+        $this->entity->expects($this->once())
+                ->method('call')
+                ->will($this->throwException(new \Exception));
+        $data = $this->plugin->update($this->entity);
+    }
+
+}
